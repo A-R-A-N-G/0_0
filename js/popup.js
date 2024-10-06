8aaa9dfba65ddd32fe5c39dbd08e1dec6d70b648
krpythondestroyer <starpeeker@naver.com>
2024-10-07 01:41:30 +0900

    풍등, 나. 부산추가

---
+++ js/popup.js
@@ -0,0 +1,11 @@
+// 팝업 표시 함수
+function showPopup() {
+    document.querySelector('.popup').style.display = 'block';
+    document.querySelector('.popup-overlay').style.display = 'block';
+}
+
+// 팝업 숨김 함수
+function hidePopup() {
+    document.querySelector('.popup').style.display = 'none';
+    document.querySelector('.popup-overlay').style.display = 'none';
+}
